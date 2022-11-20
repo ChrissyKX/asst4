fbac0f7e07cda1a1000d1886783b4a50972bc249
Proud Blessed Mpala <pmpala@myth56.stanford.edu>
2022-11-20 00:14:24 -0800

    Semi loop fusions

--- pagerank/page_rank.cpp
+++ pagerank/page_rank.cpp
@@ -51,24 +51,26 @@ void pageRank(Graph g, double* solution, double damping, double convergence)
      }
 
    */
-  int maxThreads = omp_get_max_threads();
+ 
   int numNodes = num_nodes(g);
   double equal_prob = 1.0 / numNodes;
   
   double * score_old = (double *)std::malloc(sizeof(double) * numNodes);   // old scores
   double * score_new = (double *)std::malloc(sizeof(double) * numNodes);   // new scores
-  Vertex * loners =    (Vertex *)std::malloc(sizeof(double) * numNodes);;
+  Vertex * loners =    (Vertex *)std::malloc(sizeof(double) * numNodes);
   
   int num_loners = 0;
-  #pragma omp parallel for num_threads(maxThreads)
+  #pragma omp parallel for 
   for (int v = 0; v < numNodes; v++) {
     score_old[v] = equal_prob;
     if (outgoing_size(g, v) == 0) {
-      #pragma opm atomic
-      loners[num_loners++] = v;
+      {
+        #pragma omp critical 
+        loners[num_loners++] = v;
+      }
     }
   }
-  #pragma omp barrier
+
   
   /*
      CS149 students: Implement the page rank algorithm here.  You
@@ -87,10 +89,16 @@ void pageRank(Graph g, double* solution, double damping, double convergence)
   while (!converged) {
 
     double global_diff = 0.0;
+    double sum_loners = 0.0;
+    #pragma omp parallel for reduction(+:sum_loners)
+    for (Vertex* v = loners; v < loners + num_loners; v++) {
+        sum_loners += damping * score_old[*v] / numNodes; 
+    }
     
     // compute score_new[vi] for all nodes vi:
     // score_new[vi] = sum over all nodes vj reachable from incoming edges
     //  { score_old[vj] / number of edges leaving vj  }
+    #pragma omp parallel for schedule(dynamic)
     for (int i=0; i<numNodes; i++) {
       score_new[i] = 0;
       if (incoming_size(g, i) == 0) continue;
@@ -98,42 +106,42 @@ void pageRank(Graph g, double* solution, double damping, double convergence)
       const Vertex* start = incoming_begin(g, i);
       const Vertex* end = incoming_end(g, i);
       
-      for (const Vertex* v=start; v!=end; v++)
-        score_new[i] += score_old[*v]/outgoing_size(g, *v); 
+      for (const Vertex* v=start; v!=end; v++) 
+        score_new[i] += score_old[*v]/outgoing_size(g, *v);
+      
     }
 
     
     // std::cout << "C1" << std::endl;
     // score_new[vi] = (damping * score_new[vi]) + (1.0-damping) / numNodes;
-
+    #pragma omp parallel for 
     for (int i=0; i<numNodes; i++) {
       // Vertex is typedef'ed to an int. Vertex* points into g.outgoing_edges[]
-      score_new[i] = (damping * score_new[i]) + (1.0 - damping) /  numNodes;
-    }
-
-    double sum_loners = 0.0;
-    for (Vertex* v = loners; v < loners + num_loners; v++) {
-        sum_loners += damping * score_old[*v] / numNodes; 
-    }
+   
+      score_new[i] = (damping * score_new[i]) + (1.0 - damping) /  numNodes + sum_loners;
+      /* }
     
     // std::cout << "C2" << std::endl;
     // score_new[vi] += sum over all nodes v in graph with no outgoing edges
     //                      { damping * score_old[v] / numNodes }
+    #pragma omp parallel for 
     for (int i=0; i<numNodes; i++) {
       // Vertex is typedef'ed to an int. Vertex* points into g.outgoing_edges[]
       // if (i % 1000 == 0)
         // std::cout << "Doing outter " << i << std::endl;
-      score_new[i] += sum_loners; 
+        */
+      // score_new[i] += sum_loners; 
       
     }
     // std::cout << "C3" << std::endl;
     // compute how much per-node scores have changed
     // quit once algorithm has converged
     
+
     // global_diff = sum over all nodes vi { abs(score_new[vi] - score_old[vi]) };
     
     //  }
-    
+    #pragma omp parallel for reduction(+:global_diff)
     for (int i=0; i<numNodes; i++) {
       // Vertex is typedef'ed to an int. Vertex* points into g.outgoing_edges[]
       global_diff += std::abs(score_new[i] - score_old[i]);
@@ -141,11 +149,17 @@ void pageRank(Graph g, double* solution, double damping, double convergence)
     // std::cout << global_diff << " " <<  convergence << std::endl;
     // converged = (global_diff < convergence)
     converged = global_diff < convergence;
+
+    
+    // This update should go last
+    #pragma omp parallel for 
     for (int i=0; i<numNodes; i++) {
       // Vertex is typedef'ed to an int. Vertex* points into g.outgoing_edges[]
       score_old[i] = score_new[i];
     }
+    
   }
+  #pragma omp parallel for 
   for (int i=0; i<numNodes; i++) {
     // Vertex is typedef'ed to an int. Vertex* points into g.outgoing_edges[]
     solution[i] = score_new[i];
